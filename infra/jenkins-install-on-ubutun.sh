fd3e8cff92a603e571b78860c155234ccdeb0e12
clement2019 <ayeniayokunle@yahoo.co.uk>
2024-08-25 22:30:48 +0100

    edited jenkins file

--- infra/jenkins-install-on-ubutun.sh
+++ infra/jenkins-install-on-ubutun.sh
@@ -32,6 +32,13 @@ sudo ./aws/install
 #check the version
 
 aws --version
+## install eksctl
+
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+
+sudo mv /tmp/eksctl /usr/local/bin
+
+eksctl version
 
 # install docker
 
