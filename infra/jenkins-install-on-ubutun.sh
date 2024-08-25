b6e8a2bba70393ec8013f30655ed8be4d63fa9cc
clement2019 <ayeniayokunle@yahoo.co.uk>
2024-08-25 19:09:59 +0100

    fresh files initiated

---
+++ infra/jenkins-install-on-ubutun.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# installation of git on the EC2 instance
+sudo apt update
+sudo apt install git -y
+
+# iInstall Terraform on Ubuntu 22.04|20.04 |18.04
+
+sudo apt-get update
+sudo apt install  software-properties-common gnupg2 curl
+#hwe-support-status --verbose
+curl https://apt.releases.hashicorp.com/gpg | gpg --dearmor > hashicorp.gpg
+sudo install -o root -g root -m 644 hashicorp.gpg /etc/apt/trusted.gpg.d/
+
+sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+
+sudo apt install terraform -y
+terraform --version
+
+# install kubectl on the EC2 instance
+
+sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl
+sudo chmod +x ./kubectl
+sudo mkdir -p $HOME/bin && sudo cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+
+
+
+# install docker
+
+sudo apt update -y
+
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" -y
+
+sudo apt update -y
+
+apt-cache policy docker-ce -y
+
+sudo apt install docker-ce -y
+
+#sudo systemctl status docker
+
+sudo chmod 777 /var/run/docker.sock
+
+#install jenkins
+
+sudo apt update -y
+
+sudo apt upgrade -y 
+
+sudo apt install openjdk-17-jre -y
+
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y 
+sudo apt-get install jenkins -y
--- infra/jenkins-server.tf
+++ infra/jenkins-server.tf
@@ -1,25 +1,13 @@
-data "aws_ami" "latest-amazon-linux-image" {
-  most_recent = true
-  owners      = ["amazon"]
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
 resource "aws_instance" "myapp-server" {
-  ami                         = data.aws_ami.latest-amazon-linux-image.id
+  //ami                         = data.aws_ami.instance_store_ami.id
+  ami                         = var.ami
   instance_type               = var.instance_type
   key_name                    = "devopskey2"
   subnet_id                   = aws_subnet.myapp-subnet-1.id
   vpc_security_group_ids      = [aws_default_security_group.default-sg.id]
   availability_zone           = var.avail_zone
   associate_public_ip_address = true
-  user_data                   = file("jenkins-server-script.sh")
+  user_data                   = file("jenkins-install-on-ubutun.sh")
   tags = {
     Name = "${var.env_prefix}-server"
   }
